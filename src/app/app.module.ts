07f85d8d8e34aeb943bd4452e11ffb5685e0f0ed
Atharva Mhatre <atharva.mhatre.ext@bureauveritas.com>
2024-01-23 18:56:42 +0530

    card is created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { BodyComponent } from './components/body/body.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    BodyComponent
+    BodyComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
